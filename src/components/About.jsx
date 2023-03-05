64dd737d34a1796b8b2ffbb91ff31ecf5431822d
Tanish2002Basu <>
2023-03-06 02:06:16 +0530

    added hero about and navbar

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -47,11 +47,11 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I'm a skilled software developer with experience in TypeScript and
-        JavaScript, and expertise in frameworks like React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
-        create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
+        As a highly skilled software developer, I bring extensive experience in TypeScript and JavaScript development, along with expertise in cutting-edge frameworks like React, Node.js, and Express.js. In addition, I have a deep understanding of the Database and Python, enabling me to build complex, data-driven solutions that meet your specific needs.
+
+But what really sets me apart is my ability to solve complex problems quickly and collaboratively. I pride myself on working closely with clients to deeply understand their business objectives, and then leveraging my technical expertise to create efficient, scalable, and user-friendly solutions that exceed their expectations.
+
+Whether you're looking to build a new product from scratch or optimize an existing application, I have the skills and experience to deliver results. So if you're ready to bring your ideas to life, let's work together to create something truly exceptional.
       </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10'>
