f27fb5e34eef5a0691284dc7b514148070e209ac
Tanish2002Basu <>
2023-03-05 23:43:10 +0530

    added hero about and navbar

--- src/constants/index.js
+++ src/constants/index.js
@@ -46,7 +46,7 @@ import {
       icon: web,
     },
     {
-      title: "React Native Developer",
+      title: "Problem Solver",
       icon: mobile,
     },
     {
@@ -54,7 +54,7 @@ import {
       icon: backend,
     },
     {
-      title: "Content Creator",
+      title: "UX Designer",
       icon: creator,
     },
   ];
